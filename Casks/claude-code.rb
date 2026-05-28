01530ac2e306b91c0d9b44726e7784fc4dd3a73c
ahokinson <1762048+ahokinson@users.noreply.github.com>
2026-05-28 14:20:29 +0000

    claude-code 2.1.153

--- Casks/claude-code.rb
+++ Casks/claude-code.rb
@@ -1,13 +1,13 @@
 cask "claude-code" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.1.150"
+  version "2.1.153"
 
   on_arm do
-    sha256 "2f8413ea1083f108587940496a17057751344109d261fb4239ab2d45b2285c99"
+    sha256 "449d9c89d7a63b1d427d912a7bd6e6f23f9a7b363866697c9fa9a0012546b254"
   end
   on_intel do
-    sha256 "c66d5721df38cce82cde03d244f8fa92768125fe06e8d1d38d4bfbadaf4a8d17"
+    sha256 "4b90521c64b728caabe221737ce8a83d362ef0852eee7d789f014f7ff73ce97b"
   end
 
   url "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/#{version}/darwin-#{arch}/claude",
