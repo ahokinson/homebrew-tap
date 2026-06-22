8069fddd17c6c96a1db6df78e816de1254e636e7
ahokinson <1762048+ahokinson@users.noreply.github.com>
2026-06-22 11:37:33 -0400

    Give bloom a stable release

--- Formula/bloom.rb
+++ Formula/bloom.rb
@@ -1,6 +1,8 @@
 class Bloom < Formula
   desc "Grow tmux sessions from a single config file"
   homepage "https://github.com/ahokinson/bloom"
+  url "https://github.com/ahokinson/bloom/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "860310c2edf4a42d0a1b6cda58cdb59e8943ab1a2e1a6a94537ffff180ac2fcd"
   license "MIT"
   head "https://github.com/ahokinson/bloom.git", branch: "develop"
 
@@ -12,6 +14,7 @@ class Bloom < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/bloom --version")
+    assert_match "does not exist",
+      shell_output("#{bin}/bloom /nonexistent/path", 1)
   end
 end
