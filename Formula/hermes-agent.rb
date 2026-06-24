c3bf3713cc9ea4a42800c2d8e91e34e712f07565
ahokinson <1762048+ahokinson@users.noreply.github.com>
2026-06-24 19:30:59 -0400

    Add zlib dependency for hermes-agent linkage

--- Formula/hermes-agent.rb
+++ Formula/hermes-agent.rb
@@ -13,6 +13,7 @@ class HermesAgent < Formula
   depends_on "libyaml"
   depends_on "openssl@3"
   depends_on "python@3.13"
+  depends_on "zlib"
 
   resource "annotated-doc" do
     url "https://files.pythonhosted.org/packages/57/ba/046ceea27344560984e26a590f90bc7f4a75b06701f653222458922b558c/annotated_doc-0.0.4.tar.gz"
